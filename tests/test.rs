5cc74727ce0775527e6031cffd4cf2793ebcc346
Justin de Ruiter <justinderuiter@ziggo.nl>
2024-01-11 17:17:49 +0100

    Improve test by cross checking with original C version of lzo

--- tests/test.rs
+++ tests/test.rs
@@ -1,194 +1,127 @@
 #![feature(test)]
 
-extern crate test;
-
-use std::{env, fs, path::Path};
+use std::{env, ffi::c_void, fs::File, io::Read, mem::MaybeUninit, path::Path, sync::Arc};
 
 use test::{test_main, ShouldPanic, TestDesc, TestDescAndFn, TestFn, TestName, TestType};
+use zip::ZipArchive;
+
+extern crate test;
 
 fn main() {
     let args = env::args().collect::<Vec<_>>();
 
     let mut tests = vec![];
 
-    add_corpus_tests(
-        &mut tests,
-        "tests/calgary",
-        CALGARY_COMPRESS_1_LENS,
-        CALGARY_COMPRESS_999_LENS,
-    );
-
-    add_corpus_tests(
-        &mut tests,
-        "tests/silesia",
-        SILESIA_COMPRESS_1_LENS,
-        SILESIA_COMPRESS_999_LENS,
-    );
+    add_corpus_tests(&mut tests, "corpora/calgary.zip");
+    add_corpus_tests(&mut tests, "corpora/silesia.zip");
 
     test_main(&args, tests, None);
 }
 
-fn add_corpus_tests(
-    tests: &mut Vec<TestDescAndFn>,
-    path: impl AsRef<Path>,
-    compress_1_lens: &[(&str, usize)],
-    compress_999_lens: &[(&str, usize)],
-) {
-    for entry in fs::read_dir(path).unwrap() {
-        let entry = entry.unwrap();
+fn add_corpus_tests(tests: &mut Vec<TestDescAndFn>, path: impl AsRef<Path>) {
+    let file = File::open(path).unwrap();
+    let mut zip_archive = ZipArchive::new(file).unwrap();
 
-        let file_name = entry.file_name().to_str().unwrap().to_owned();
+    for index in 0..zip_archive.len() {
+        let mut file = zip_archive.by_index(index).unwrap();
 
-        let compress_1_len = compress_1_lens
-            .iter()
-            .find(|(name, _)| name == &file_name)
-            .unwrap()
-            .1;
+        if file.is_dir() {
+            continue;
+        }
 
-        let path = entry.path();
+        let mut buf = Vec::with_capacity(file.size() as usize);
+        file.read_to_end(&mut buf).unwrap();
 
-        let test = TestDescAndFn {
-            desc: TestDesc {
-                name: TestName::DynTestName(format!("lzo1x-1 {file_name}")),
-                ignore: false,
-                ignore_message: None,
-                source_file: "",
-                start_line: 0,
-                start_col: 0,
-                end_line: 0,
-                end_col: 0,
-                should_panic: ShouldPanic::No,
-                compile_fail: false,
-                no_run: false,
-                test_type: TestType::IntegrationTest,
-            },
-            testfn: TestFn::DynTestFn(Box::new(move || {
-                test_roundtrip_1(path, compress_1_len);
+        let data = Arc::new(buf);
+        let data_ref = Arc::clone(&data);
+
+        let test = create_test(
+            format!("roundtrip 1 {}", file.name()),
+            Box::new(move || {
+                let compressed = lzo1x::compress_1(&data_ref);
+
+                assert!(compressed == lzo_sys_compress_1(&data_ref));
+
+                let mut decompressed = vec![0; data_ref.len()];
+                lzo1x::decompress(&compressed, &mut decompressed);
+
+                assert!(decompressed == *data_ref);
 
                 Ok(())
-            })),
-        };
+            }),
+        );
 
         tests.push(test);
 
-        let compress_999_len = compress_999_lens
-            .iter()
-            .find(|(name, _)| name == &file_name)
-            .unwrap()
-            .1;
+        let test = create_test(
+            format!("roundtrip 999 {}", file.name()),
+            Box::new(move || {
+                let compressed = lzo1x::compress_999(&data);
 
-        let test = TestDescAndFn {
-            desc: TestDesc {
-                name: TestName::DynTestName(format!("lzo1x-999 {file_name}")),
-                ignore: false,
-                ignore_message: None,
-                source_file: "",
-                start_line: 0,
-                start_col: 0,
-                end_line: 0,
-                end_col: 0,
-                should_panic: ShouldPanic::No,
-                compile_fail: false,
-                no_run: false,
-                test_type: TestType::IntegrationTest,
-            },
-            testfn: TestFn::DynTestFn(Box::new(move || {
-                test_roundtrip_999(entry.path(), compress_999_len);
+                assert!(compressed == lzo_sys_compress_999(&data));
+
+                let mut decompressed = vec![0; data.len()];
+                lzo1x::decompress(&compressed, &mut decompressed);
+
+                assert!(decompressed == *data);
 
                 Ok(())
-            })),
-        };
+            }),
+        );
 
         tests.push(test);
     }
 }
 
-fn test_roundtrip_1(path: impl AsRef<Path>, compressed_len: usize) {
-    let data = fs::read(path.as_ref()).unwrap();
-
-    let compressed = lzo1x::compress_1(&data);
-
-    assert_eq!(compressed.len(), compressed_len);
-
-    let mut decompressed = vec![0; data.len()];
-    lzo1x::decompress(&compressed, &mut decompressed);
-
-    assert!(decompressed == data);
+fn create_test(
+    name: String,
+    test_fn: Box<dyn FnOnce() -> Result<(), String> + Send>,
+) -> TestDescAndFn {
+    TestDescAndFn {
+        desc: TestDesc {
+            name: TestName::DynTestName(name),
+            ignore: false,
+            ignore_message: None,
+            source_file: "",
+            start_line: 0,
+            start_col: 0,
+            end_line: 0,
+            end_col: 0,
+            should_panic: ShouldPanic::No,
+            compile_fail: false,
+            no_run: false,
+            test_type: TestType::IntegrationTest,
+        },
+        testfn: TestFn::DynTestFn(test_fn),
+    }
 }
 
-fn test_roundtrip_999(path: impl AsRef<Path>, compressed_len: usize) {
-    let data = fs::read(path.as_ref()).unwrap();
-
-    let compressed = lzo1x::compress_999(&data);
-
-    assert_eq!(compressed.len(), compressed_len);
-
-    let mut decompressed = vec![0; data.len()];
-    lzo1x::decompress(&compressed, &mut decompressed);
-
-    assert!(decompressed == data);
+fn lzo_sys_compress_1(src: &[u8]) -> Vec<u8> {
+    lzo_sys_compress(src, lzo_sys::lzo1x::lzo1x_1_compress)
 }
 
-const CALGARY_COMPRESS_1_LENS: &[(&str, usize)] = &[
-    ("bib", 58272),
-    ("book1", 495312),
-    ("book2", 331307),
-    ("geo", 100499),
-    ("news", 215998),
-    ("obj1", 12858),
-    ("obj2", 117622),
-    ("paper1", 28072),
-    ("paper2", 46599),
-    ("pic", 87126),
-    ("progc", 19736),
-    ("progl", 26586),
-    ("progp", 17296),
-    ("trans", 32241),
-];
+fn lzo_sys_compress_999(src: &[u8]) -> Vec<u8> {
+    lzo_sys_compress(src, lzo_sys::lzo1x::lzo1x_999_compress)
+}
 
-const SILESIA_COMPRESS_1_LENS: &[(&str, usize)] = &[
-    ("dickens", 6207702),
-    ("mozilla", 25451668),
-    ("mr", 5335246),
-    ("nci", 6282199),
-    ("ooffice", 4159595),
-    ("osdb", 5658863),
-    ("reymont", 3170859),
-    ("samba", 8017488),
-    ("sao", 6469903),
-    ("webster", 20036443),
-    ("x-ray", 8497133),
-    ("xml", 1292820),
-];
+fn lzo_sys_compress(src: &[u8], compress_fn: lzo_sys::lzoconf::lzo_compress_t) -> Vec<u8> {
+    let mut dst = vec![0; src.len() + (src.len() / 16) + 64 + 3];
+    let mut dst_len = MaybeUninit::uninit();
+    let mut wrkmem = vec![0; lzo_sys::lzo1x::LZO1X_1_MEM_COMPRESS as usize];
 
-const CALGARY_COMPRESS_999_LENS: &[(&str, usize)] = &[
-    ("bib", 39332),
-    ("book1", 356719),
-    ("book2", 232407),
-    ("geo", 74235),
-    ("news", 160298),
-    ("obj1", 11170),
-    ("obj2", 88666),
-    ("paper1", 20946),
-    ("paper2", 33893),
-    ("pic", 64504),
-    ("progc", 15383),
-    ("progl", 18743),
-    ("progp", 12913),
-    ("trans", 21608),
-];
+    unsafe {
+        compress_fn(
+            src.as_ptr(),
+            src.len(),
+            dst.as_mut_ptr(),
+            dst_len.as_mut_ptr(),
+            wrkmem.as_mut_ptr() as *mut c_void,
+        )
+    };
 
-const SILESIA_COMPRESS_999_LENS: &[(&str, usize)] = &[
-    ("dickens", 4402291),
-    ("mozilla", 20705180),
-    ("mr", 4044194),
-    ("nci", 3789816),
-    ("ooffice", 3312800),
-    ("osdb", 4042468),
-    ("reymont", 2125875),
-    ("samba", 5880966),
-    ("sao", 5771688),
-    ("webster", 13693900),
-    ("x-ray", 6767197),
-    ("xml", 765173),
-];
+    let dst_len = unsafe { dst_len.assume_init() };
+
+    dst.resize(dst_len, 0);
+
+    dst
+}
