52574ddd898c3b236f87e6c7af14ab742156b7ee
Justin de Ruiter <justinderuiter@ziggo.nl>
2024-01-13 10:37:51 +0100

    Cleanup find_match_len code

--- src/compress_1.rs
+++ src/compress_1.rs
@@ -1,4 +1,5 @@
 use alloc::{vec, vec::Vec};
+use cfg_if::cfg_if;
 
 use crate::config::{
     M2_MAX_LEN, M2_MAX_OFFSET, M3_MARKER, M3_MAX_LEN, M3_MAX_OFFSET, M4_MARKER, M4_MAX_LEN,
@@ -10,8 +11,8 @@ pub fn compress_1(src: &[u8], d_bits: u32) -> Vec<u8> {
     let mut dst = vec![0; src_len + (src_len / 16) + 64 + 3];
     let mut work_mem = vec![0; 1 << d_bits];
 
-    let mut src_idx = 0;
-    let mut dst_idx = 0;
+    let mut src_pos = 0;
+    let mut dst_pos = 0;
     let mut l = src_len;
     let mut t = 0;
 
@@ -25,180 +26,148 @@ pub fn compress_1(src: &[u8], d_bits: u32) -> Vec<u8> {
         work_mem.fill(0);
 
         let (new_t, out_len) = {
-            let src_start = src_idx;
+            let src_start = src_pos;
             let src_len = ll;
-            let dst_start = dst_idx;
+            let src_pos_end = src_start + src_len - 20;
+            let dst_start = dst_pos;
             let dict = &mut work_mem;
 
-            let mut src_idx = src_start;
-            let mut dst_idx = dst_idx;
+            let mut src_pos = src_start;
+            let mut dst_pos = dst_pos;
             let mut ti = t;
-            let mut ii = src_idx;
+            let mut ii = src_pos;
 
             if ti < 4 {
-                src_idx += 4 - ti;
+                src_pos += 4 - ti;
             }
 
-            src_idx += 1 + ((src_idx - ii) >> 5);
+            src_pos += 1 + ((src_pos - ii) >> 5);
 
             'main_loop: loop {
-                let mut m_pos;
+                let mut match_pos;
 
                 loop {
-                    if src_idx >= src_start + src_len - 20 {
+                    if src_pos >= src_pos_end {
                         break 'main_loop;
                     }
 
-                    let dv = u32::from_le_bytes(src[src_idx..src_idx + 4].try_into().unwrap());
+                    let dv = get_u32_le(src, src_pos);
                     let dindex = (((0x1824429du32.wrapping_mul(dv)) >> (32 - d_bits))
                         & ((1 << d_bits) - 1)) as usize;
-                    m_pos = src_start + dict[dindex] as usize;
-                    dict[dindex] = (src_idx - src_start) as u16;
+                    match_pos = src_start + dict[dindex] as usize;
+                    dict[dindex] = (src_pos - src_start) as u16;
 
-                    if dv == u32::from_le_bytes(src[m_pos..m_pos + 4].try_into().unwrap()) {
+                    if dv == get_u32_le(src, match_pos) {
                         break;
                     }
 
-                    src_idx += 1 + ((src_idx - ii) >> 5);
+                    src_pos += 1 + ((src_pos - ii) >> 5);
                 }
 
                 ii -= ti;
                 ti = 0;
-                let t = src_idx - ii;
+                let t = src_pos - ii;
 
                 match t {
                     0..=3 => {
-                        dst[dst_idx - 2] |= t as u8;
+                        dst[dst_pos - 2] |= t as u8;
                     }
                     4..=18 => {
-                        dst[dst_idx] = t as u8 - 3;
-                        dst_idx += 1;
+                        dst[dst_pos] = t as u8 - 3;
+                        dst_pos += 1;
                     }
                     19.. => {
                         let mut tt = t - 18;
-                        dst[dst_idx] = 0;
-                        dst_idx += 1;
+                        dst[dst_pos] = 0;
+                        dst_pos += 1;
 
                         while tt > 255 {
                             tt -= 255;
-                            dst[dst_idx] = 0;
-                            dst_idx += 1;
+                            dst[dst_pos] = 0;
+                            dst_pos += 1;
                         }
 
-                        dst[dst_idx] = tt as u8;
-                        dst_idx += 1;
+                        dst[dst_pos] = tt as u8;
+                        dst_pos += 1;
                     }
                 }
 
-                dst[dst_idx..dst_idx + t].copy_from_slice(&src[ii..ii + t]);
-                dst_idx += t;
+                dst[dst_pos..dst_pos + t].copy_from_slice(&src[ii..ii + t]);
+                dst_pos += t;
 
-                let mut m_len = 4;
+                let mut match_len = find_match_len(src, src_pos, src_pos_end, match_pos);
 
-                loop {
-                    let a = u64::from_ne_bytes(
-                        src[src_idx + m_len..src_idx + m_len + 8]
-                            .try_into()
-                            .unwrap(),
-                    );
+                let mut match_off = src_pos - match_pos;
+                src_pos += match_len;
+                ii = src_pos;
 
-                    let b = u64::from_ne_bytes(
-                        src[m_pos + m_len..m_pos + m_len + 8].try_into().unwrap(),
-                    );
+                if match_len <= M2_MAX_LEN && match_off <= M2_MAX_OFFSET {
+                    match_off -= 1;
+                    dst[dst_pos] = (((match_len - 1) << 5) | ((match_off & 7) << 2)) as u8;
+                    dst_pos += 1;
+                    dst[dst_pos] = (match_off >> 3) as u8;
+                    dst_pos += 1;
+                } else if match_off <= M3_MAX_OFFSET {
+                    match_off -= 1;
 
-                    let v = a ^ b;
-
-                    if v == 0 {
-                        m_len += 8;
-
-                        if src_idx + m_len >= src_start + src_len - 20 {
-                            break;
-                        }
+                    if match_len <= M3_MAX_LEN {
+                        dst[dst_pos] = (M3_MARKER | (match_len - 2)) as u8;
+                        dst_pos += 1;
                     } else {
-                        #[cfg(target_endian = "little")]
-                        {
-                            m_len += v.trailing_zeros() as usize / 8;
+                        match_len -= M3_MAX_LEN;
+                        dst[dst_pos] = M3_MARKER as u8;
+                        dst_pos += 1;
+
+                        while match_len > 255 {
+                            match_len -= 255;
+                            dst[dst_pos] = 0;
+                            dst_pos += 1;
                         }
 
-                        #[cfg(target_endian = "big")]
-                        {
-                            m_len += v.leading_zeros() as usize / 8;
-                        }
-
-                        break;
-                    }
-                }
-
-                let mut m_off = src_idx - m_pos;
-                src_idx += m_len;
-                ii = src_idx;
-
-                if m_len <= M2_MAX_LEN && m_off <= M2_MAX_OFFSET {
-                    m_off -= 1;
-                    dst[dst_idx] = (((m_len - 1) << 5) | ((m_off & 7) << 2)) as u8;
-                    dst_idx += 1;
-                    dst[dst_idx] = (m_off >> 3) as u8;
-                    dst_idx += 1;
-                } else if m_off <= M3_MAX_OFFSET {
-                    m_off -= 1;
-
-                    if m_len <= M3_MAX_LEN {
-                        dst[dst_idx] = (M3_MARKER | (m_len - 2)) as u8;
-                        dst_idx += 1;
-                    } else {
-                        m_len -= M3_MAX_LEN;
-                        dst[dst_idx] = M3_MARKER as u8;
-                        dst_idx += 1;
-
-                        while m_len > 255 {
-                            m_len -= 255;
-                            dst[dst_idx] = 0;
-                            dst_idx += 1;
-                        }
-
-                        dst[dst_idx] = m_len as u8;
-                        dst_idx += 1;
+                        dst[dst_pos] = match_len as u8;
+                        dst_pos += 1;
                     }
 
-                    dst[dst_idx] = (m_off << 2) as u8;
-                    dst_idx += 1;
-                    dst[dst_idx] = (m_off >> 6) as u8;
-                    dst_idx += 1;
+                    dst[dst_pos] = (match_off << 2) as u8;
+                    dst_pos += 1;
+                    dst[dst_pos] = (match_off >> 6) as u8;
+                    dst_pos += 1;
                 } else {
-                    m_off -= 0x4000;
+                    match_off -= 0x4000;
 
-                    if m_len <= M4_MAX_LEN {
-                        dst[dst_idx] = (M4_MARKER | ((m_off >> 11) & 8) | (m_len - 2)) as u8;
-                        dst_idx += 1;
+                    if match_len <= M4_MAX_LEN {
+                        dst[dst_pos] =
+                            (M4_MARKER | ((match_off >> 11) & 8) | (match_len - 2)) as u8;
+                        dst_pos += 1;
                     } else {
-                        m_len -= M4_MAX_LEN;
-                        dst[dst_idx] = (M4_MARKER | ((m_off >> 11) & 8)) as u8;
-                        dst_idx += 1;
+                        match_len -= M4_MAX_LEN;
+                        dst[dst_pos] = (M4_MARKER | ((match_off >> 11) & 8)) as u8;
+                        dst_pos += 1;
 
-                        while m_len > 255 {
-                            m_len -= 255;
-                            dst[dst_idx] = 0;
-                            dst_idx += 1;
+                        while match_len > 255 {
+                            match_len -= 255;
+                            dst[dst_pos] = 0;
+                            dst_pos += 1;
                         }
 
-                        dst[dst_idx] = m_len as u8;
-                        dst_idx += 1;
+                        dst[dst_pos] = match_len as u8;
+                        dst_pos += 1;
                     }
 
-                    dst[dst_idx] = (m_off << 2) as u8;
-                    dst_idx += 1;
-                    dst[dst_idx] = (m_off >> 6) as u8;
-                    dst_idx += 1;
+                    dst[dst_pos] = (match_off << 2) as u8;
+                    dst_pos += 1;
+                    dst[dst_pos] = (match_off >> 6) as u8;
+                    dst_pos += 1;
                 }
             }
 
-            ((src_start + src_len) - (ii - ti), dst_idx - dst_start)
+            ((src_start + src_len) - (ii - ti), dst_pos - dst_start)
         };
 
         t = new_t;
 
-        src_idx += ll;
-        dst_idx += out_len;
+        src_pos += ll;
+        dst_pos += out_len;
         l -= ll;
     }
 
@@ -207,41 +176,139 @@ pub fn compress_1(src: &[u8], d_bits: u32) -> Vec<u8> {
     if t > 0 {
         let ii = src_len - t;
 
-        if dst_idx == 0 && t <= 238 {
-            dst[dst_idx] = 17 + t as u8;
-            dst_idx += 1;
+        if dst_pos == 0 && t <= 238 {
+            dst[dst_pos] = 17 + t as u8;
+            dst_pos += 1;
         } else if t <= 3 {
-            dst[dst_idx - 2] |= t as u8;
+            dst[dst_pos - 2] |= t as u8;
         } else if t <= 18 {
-            dst[dst_idx] = t as u8 - 3;
-            dst_idx += 1;
+            dst[dst_pos] = t as u8 - 3;
+            dst_pos += 1;
         } else {
             let mut tt = t - 18;
 
-            dst[dst_idx] = 0;
-            dst_idx += 1;
+            dst[dst_pos] = 0;
+            dst_pos += 1;
 
             while tt > 255 {
                 tt -= 255;
-                dst[dst_idx] = 0;
-                dst_idx += 1;
+                dst[dst_pos] = 0;
+                dst_pos += 1;
             }
 
-            dst[dst_idx] = tt as u8;
-            dst_idx += 1;
+            dst[dst_pos] = tt as u8;
+            dst_pos += 1;
         }
 
-        dst[dst_idx..dst_idx + t].copy_from_slice(&src[ii..ii + t]);
-        dst_idx += t;
+        dst[dst_pos..dst_pos + t].copy_from_slice(&src[ii..ii + t]);
+        dst_pos += t;
     }
 
-    dst[dst_idx] = (M4_MARKER | 1) as u8;
-    dst_idx += 1;
-    dst[dst_idx] = 0;
-    dst_idx += 1;
-    dst[dst_idx] = 0;
-    dst_idx += 1;
+    dst[dst_pos] = (M4_MARKER | 1) as u8;
+    dst_pos += 1;
+    dst[dst_pos] = 0;
+    dst_pos += 1;
+    dst[dst_pos] = 0;
+    dst_pos += 1;
 
-    dst.resize(dst_idx, 0);
+    dst.resize(dst_pos, 0);
     dst
+}
+
+cfg_if! {
+    if #[cfg(target_pointer_width = "64")] {
+        fn find_match_len(src: &[u8], src_pos: usize, src_pos_end: usize, match_pos: usize) -> usize {
+            let mut match_len = 4;
+
+            loop {
+                let v = get_u64_ne(src, src_pos + match_len) ^ get_u64_ne(src, match_pos + match_len);
+
+                if v != 0 {
+                    cfg_if! {
+                        if #[cfg(target_endian = "little")] {
+                            match_len += v.trailing_zeros() as usize / 8;
+                        } else if #[cfg(target_endian = "big")] {
+                            match_len += v.leading_zeros() as usize / 8;
+                        }
+                    }
+
+                    break;
+                }
+
+                match_len += 8;
+
+                if src_pos + match_len >= src_pos_end {
+                    break;
+                }
+            }
+
+            match_len
+        }
+    } else if #[cfg(target_pointer_width = "32")] {
+        fn find_match_len(src: &[u8], src_pos: usize, src_pos_end: usize, match_pos: usize) -> usize {
+            let mut match_len = 4;
+
+            loop {
+                for _ in 0..2 {
+                    let v = get_u32_ne(src, src_pos + match_len) ^ get_u32_ne(src, match_pos + match_len);
+
+                    if v != 0 {
+                        cfg_if! {
+                            if #[cfg(target_endian = "little")] {
+                                match_len += v.trailing_zeros() as usize / 8;
+                            } else if #[cfg(target_endian = "big")] {
+                                match_len += v.leading_zeros() as usize / 8;
+                            }
+                        }
+
+                        break;
+                    }
+
+                    match_len += 4;
+                }
+
+                if src_pos + match_len >= src_pos_end {
+                    break;
+                }
+            }
+
+            match_len
+        }
+    } else {
+        fn find_match_len(src: &[u8], src_pos: usize, src_pos_end: usize, match_pos: usize) -> usize {
+            let mut match_len = 4;
+
+            'main_loop: loop {
+                for _ in 0..8 {
+                    if src[src_pos + match_len] != src[match_pos + match_len] {
+                        break 'main_loop;
+                    }
+
+                    match_len += 1;
+                }
+
+                if src_pos + match_len >= src_pos_end {
+                    break;
+                }
+            }
+
+            match_len
+        }
+    }
+}
+
+fn get_u32_le(src: &[u8], src_pos: usize) -> u32 {
+    u32::from_le_bytes(src[src_pos..src_pos + 4].try_into().unwrap())
+}
+
+cfg_if! {
+    if #[cfg(target_pointer_width = "64")] {
+        fn get_u64_ne(src: &[u8], src_pos: usize) -> u64 {
+            u64::from_ne_bytes(src[src_pos..src_pos + 8].try_into().unwrap())
+        }
+    } else if #[cfg(target_pointer_width = "32")] {
+        fn get_u32_ne(src: &[u8], src_pos: usize) -> u32 {
+            u32::from_ne_bytes(src[src_pos..src_pos + 4].try_into().unwrap())
+        }
+    }
 }
